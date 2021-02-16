a318dc64b620c46769493037b8b2b5067bd580d9
Kazuki IWANAGA <47914321+kazuki-iwanaga@users.noreply.github.com>
2021-02-17 01:25:48 +0900

    Added sample http server

---
+++ .bashrc
@@ -0,0 +1,4 @@
+export TZ=Asia/Tokyo
+export LANG=en_US.UTF-8
+export HISTSIZE=10000
+export PS1='[\t \[\e[1;32m\]jupyroot\[\e[0m\] \W]\$ '
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+// https://github.com/seanmonstar/warp
+
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let hello = warp::path!("hello" / String)
+        .map(|name| format!("Hello, {}!", name));
+    
+    warp::serve(hello)
+        .run(([0, 0, 0, 0], 8989))
+        .await;
 }
