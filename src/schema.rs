0e53022dcfe7fc47a1bcac926313d02c19b1dfd0
Kazuki IWANAGA <47914321+kazuki-iwanaga@users.noreply.github.com>
2021-02-21 21:55:07 +0900

    Added diesel_cli configuration

---
+++ src/schema.rs
@@ -0,0 +1,28 @@
+table! {
+    papers (paper_id) {
+        paper_id -> Integer,
+        paper_title -> Varchar,
+        paper_author -> Nullable<Varchar>,
+        paper_year -> Nullable<Integer>,
+        user_id -> Nullable<Integer>,
+        created_at -> Datetime,
+        updated_at -> Datetime,
+    }
+}
+
+table! {
+    users (user_id) {
+        user_id -> Integer,
+        user_name -> Varchar,
+        user_email -> Nullable<Varchar>,
+        created_at -> Datetime,
+        updated_at -> Datetime,
+    }
+}
+
+joinable!(papers -> users (user_id));
+
+allow_tables_to_appear_in_same_query!(
+    papers,
+    users,
+);
