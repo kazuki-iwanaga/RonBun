2515301d9ea1ea9f193f11b4bb1e0be68007866a
KodamaYuga <yk410320@gmail.com>
2021-03-11 14:28:46 +0900

    Modify papers/up.sql, users/up.sql

--- migrations/2021-02-21-121718_papers/up.sql
+++ migrations/2021-02-21-121718_papers/up.sql
@@ -1,15 +1,15 @@
 CREATE TABLE IF NOT EXISTS `papers` (
     `paper_id`      INT(10) AUTO_INCREMENT PRIMARY KEY,
     `paper_title`   VARCHAR(100) NOT NULL,
-    `paper_author`  VARCHAR(50),
-    `paper_year`    INT(4),
-    `user_id`       INT(10),
+    `paper_author`  VARCHAR(50) NOT NULL,
+    `paper_year`    INT(4) NOT NULL,
+    `user_id`       INT(10) NOT NULL,
     `created_at`    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at`    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `updated_at`    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 
-    FOREIGN KEY (user_id)
-        REFERENCES users (user_id)
-        ON UPDATE CASCADE ON DELETE SET NULL
+    -- FOREIGN KEY (user_id)
+    --     REFERENCES users (user_id)
+    --     ON UPDATE CASCADE ON DELETE SET NULL
 ) ENGINE=INNODB DEFAULT CHARSET=utf8;
 
 -- Sample Data --
@@ -19,4 +19,5 @@ INSERT INTO papers (paper_title, paper_author, paper_year, user_id) VALUES
 ('たんぽぽの育て方', 'たんぽぽ伯爵', 2018, 1),
 ('The effective way to quit your university.', 'Taro Yamada', 2020, 2),
 ('シャープペンシルは本当にシャープか', '山田太郎', 2001, 2),
-('What is Tanpopo?', 'Count Tanpopo', 1999, 1);
+('What is Tanpopo?', 'Count Tanpopo', 1999, 1),
+('春休みに食べる魚の種類', '山田太郎', 2021, 2);
